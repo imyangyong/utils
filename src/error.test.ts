6053b136625e37ca54c68abb0a8415c96a386883
杨勇 <1994101600@qq.com>
2023-01-21 17:32:37 +0800

    feat: error base

---
+++ src/error.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest'
+import { ErrorBase } from './error'
+
+type ErrorName =
+  | 'GET_PROJECT_ERROR'
+  | 'CREATE_PROJECT_ERROR'
+  | 'UPDATE_PROJECT_ERROR'
+  | 'DELETE_PROJECT_ERROR'
+
+class TeamError extends ErrorBase<ErrorName> {}
+
+describe('error', () => {
+  it('should be able to create an error', () => {
+    try {
+      throw new TeamError({
+        name: 'GET_PROJECT_ERROR',
+        message: 'Failed to get project',
+      })
+    }
+    catch (error) {
+      if (error instanceof TeamError) {
+        expect(error.name).toBe('GET_PROJECT_ERROR')
+        expect(error.message).toBe('Failed to get project')
+        expect(error.cause).toBeUndefined()
+      }
+    }
+  })
+})
