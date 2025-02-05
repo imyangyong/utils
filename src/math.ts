ac9188768ecb80baaee09d815343fb12bcc7f4b1
杨勇 <1994101600@qq.com>
2025-02-05 11:39:47 +0800

    fix: 消除循环引用

--- src/math.ts
+++ src/math.ts
@@ -1,11 +1,10 @@
-import { flattenArrayable } from './array'
-
 export function clamp(n: number, min: number, max: number) {
   return Math.min(max, Math.max(min, n))
 }
 
 export function sum(...args: number[] | number[][]) {
-  return flattenArrayable(args).reduce((a, b) => a + b, 0)
+  const array = Array.isArray(args) ? args : [args]
+  return array.flat(1).reduce((a, b) => a + b, 0)
 }
 
 /**
