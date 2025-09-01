4bfec7c466beca67a5c978f5ee3b67d76b379b46
杨勇 <1994101600@qq.com>
2025-09-01 18:15:20 +0800

    feat: isElementVisible

---
+++ src/dom.ts
@@ -0,0 +1,7 @@
+export function isElementVisible(element: HTMLElement): boolean {
+  const style = window.getComputedStyle(element);
+  if (style.position === 'fixed' || element.tagName === 'BODY' || element.tagName === 'HTML') {
+    return style.display !== 'none' && style.visibility !== 'hidden';
+  }
+  return element.offsetParent !== null;
+}
